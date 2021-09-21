252ce572a05fb43f82bcba031e5f90fce097937c
Kenneth Johnson C <kennojc@gmail.com>
2021-09-20 23:09:35 -0300

    Added JQuery and Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/index'
   devise_for :users
   resources :books
-  root to: "books#index"
+  root to: "pages#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
